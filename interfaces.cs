cb04fdd64c4db1576454d6bda67a71550c2da473
Tanuj1609 <143291683+Tanuj1609@users.noreply.github.com>
2024-12-09 13:25:08 +0530

    Add files via upload

---
+++ interfaces.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning
+{
+    public interface Idrawable
+    {
+        void draw();
+    }
+    public class Circle : Idrawable
+    {
+        public void draw()
+        {
+            Console.WriteLine("draw a circle");
+        }
+    }
+
+    public class Rectangle : Idrawable
+
+    {
+        public void draw()
+        {
+            Console.WriteLine("draw a reactangle");
+        }
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Idrawable d = new Circle();
+            Idrawable k = new Rectangle();
+
+            d.draw();
+            k.draw();
+        }
+    }
+}
