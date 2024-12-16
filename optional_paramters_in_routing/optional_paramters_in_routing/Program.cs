6792ccb9ee97a2b94a8a98096a7bd39d3d1f46f5
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-16 15:34:57 +0530

    learnt optional parameters in routing

---
+++ optional_paramters_in_routing/optional_paramters_in_routing/Program.cs
@@ -0,0 +1,45 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+//enable routing
+app.UseRouting();
+
+//creating endpoints
+app.UseEndpoints(endpoints =>
+{
+    endpoints.Map("files/{filename}.{extension}", async (context) =>
+    {
+        string? filename = Convert.ToString(context.Request.RouteValues["filename"]);
+        string? extension = Convert.ToString(context.Request.RouteValues["extension"]);
+        await context.Response.WriteAsync($"In files - {filename} - {extension}");
+    });
+
+
+    endpoints.Map("employee/{employeename=THANOS}", async (context) =>
+    {
+        string? employeename = Convert.ToString(context.Request.RouteValues["employeename"]);
+
+        await context.Response.WriteAsync($"In employee section - {employeename}");
+    });
+
+    endpoints.Map("product/detail/{id?}", async (context) =>
+    {
+        if (context.Request.RouteValues.ContainsKey("id"))
+        {
+            int id = Convert.ToInt32(context.Request.RouteValues["id"]);
+
+            await context.Response.WriteAsync($"Product id is - {id}");
+        }
+
+        else
+        {
+            await context.Response.WriteAsync($"Product id is not supplied");
+        }
+    });
+});
+
+app.Run(async context => {
+    await context.Response.WriteAsync($"Request received at {context.Request.Path}");
+});
+
+app.Run();
