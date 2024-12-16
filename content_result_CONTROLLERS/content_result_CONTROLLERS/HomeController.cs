990255050f9165d63d9fd652f4b47051ebf29e8b
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-16 20:47:07 +0530

    learnt about contentresult

---
+++ content_result_CONTROLLERS/content_result_CONTROLLERS/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
+namespace multiple_action_methods_in_controllers
+{
+    [Controller] //used as a convention, not manatory, if controller suffixed in class name
+    public class Home:Controller
+    {
+        [Route("/")]
+        [Route("Index")]
+        public ContentResult Index()
+        {
+            //return Content("Hello from Thanos", "text/plain");
+            return Content("<h1>Hello from Thanos</h1> <h2>hello from index</h2>", "text/html");
+        }
+
+        [Route("About")]
+        public string About()
+        {
+            return ("Hello from About");
+        }
+
+        [Route("Contact/{mobile:regex(^\\d{{10}}$)}")]
+        public string Contact()
+        {
+            return ("Hello from Contact");
+        }
+    }
+}
