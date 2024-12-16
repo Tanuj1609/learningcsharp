18d60dbabc69d3e4377dbb0714d356ceb303436a
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-16 21:06:31 +0530

    learnt about JSONresult

---
+++ JSON_result_CONTROLLERS/JSON_result_CONTROLLERS/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
+using JSON_result_CONTROLLERS.models;
+
+namespace multiple_action_methods_in_controllers
+{
+    [Controller] //used as a convention, not manatory, if controller suffixed in class name
+    public class Home : Controller
+    {
+        [Route("/")]
+        [Route("Index")]
+        public ContentResult Index()
+        {
+            //return Content("Hello from Thanos", "text/plain");
+            return Content("<h1>Hello from Thanos</h1> <h2>hello from index</h2>", "text/html");
+        }
+
+        [Route("person")]
+        public JsonResult Person()
+        {
+            Person person = new Person()
+            {
+                id = Guid.NewGuid(),
+                firstname = "Thanos",
+                lastname = "Tew",
+                age = 30
+            };
+            return Json(person);
+        }
+
+        [Route("Contact/{mobile:regex(^\\d{{10}}$)}")]
+        public string Contact()
+        {
+            return ("Hello from Contact");
+        }
+    }
+}
---
+++ JSON_result_CONTROLLERS/JSON_result_CONTROLLERS/models/person.cs
@@ -0,0 +1,10 @@
+﻿namespace JSON_result_CONTROLLERS.models
+{
+    public class Person
+    {
+        public Guid id { get; set; }
+        public string? firstname { get; set; }
+        public string? lastname { get; set; }
+        public int age { get; set; }
+    }
+}
