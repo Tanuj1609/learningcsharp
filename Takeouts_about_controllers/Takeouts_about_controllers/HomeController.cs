a8abaede7694fb3b456ca4204c54b74a5465eb83
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-16 20:26:01 +0530

    leant basic conventions in controllers

---
+++ Takeouts_about_controllers/Takeouts_about_controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace multiple_action_methods_in_controllers
+{
+    [Controller] //used as a convention, not manatory, if controller suffixed in class name
+    public class Home
+    {
+        [Route("/")]
+        [Route("Index")]
+        public string Index()
+        {
+            return ("Hello from index");
+        }
+
+        [Route("About")]
+        public string About()
+        {
+            return ("Hello from About");
+        }
+
+        [Route("Contact/{mobile:regex(^\\d{{10}}$)}")]
+        public string Contact()
+        {
+            return ("Hello from Contact");
+        }
+    }
+}
