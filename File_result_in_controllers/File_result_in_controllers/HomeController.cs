a5db7a1cbd7e8df24010d589e7814080ca29a758
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-17 13:57:09 +0530

    learnt types of file results

---
+++ File_result_in_controllers/File_result_in_controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace File_result_in_controllers
+{
+    [Controller]
+    public class HomeController: Controller
+    {
+        [Route("file-download")]
+
+        public VirtualFileResult Filedownload()
+        {
+            return File("/dummypdf.pdf", "application/pdf");
+        }
+
+        [Route("file-download2")]
+
+        public PhysicalFileResult Filedownload2()
+        {
+            //return new PhysicalFileResult("C:\\Users\\TanujTewatia\\Desktop\\dummypdf.pdf", "application/pdf");
+            return PhysicalFile("C:\\Users\\TanujTewatia\\Desktop\\dummypdf.pdf", "application/pdf");
+        }
+    }
+}
