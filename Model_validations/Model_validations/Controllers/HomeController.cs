9fb716d762ee86a000be72a31aafe12d6a91fd5b
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-19 18:43:51 +0530

    intro to model validations

---
+++ Model_validations/Model_validations/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Model_validations.models;
+
+namespace Model_validations.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route ("register")]
+        public IActionResult Index(persons person)
+        {
+            return Content($"{ person}");
+        }
+    }
+}
