a2101b7aa4d10c8de5be3dbfb46967c7be5b4186
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-19 19:04:20 +0530

    learnt 3 model states isvalid, error and no of errors

---
+++ Model_states/Model_states/Models/persons.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace Model_validations.models
+{
+    public class persons
+    {
+        [Required]
+        public string? PersonName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public double? Price { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person object- Person name: {PersonName},Email: {Email}, Phone: {Phone}," +
+                $"Password: {Password}, Confirm Password: {ConfirmPassword}" +
+                $"Price: {Price}";
+        }
+
+
+    }
+}
