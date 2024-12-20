5cd33af2bb47ea0da6e941353875e177a0ce9646
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-20 20:35:07 +0530

    used switch statement in razor views

---
+++ switch_statement_in_RV/switch_statement_in_RV/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace codeBlocks_and_expressions.Models
+{
+    public class Person
+    {
+        public string? Name { get; set; }
+        public DateTime? dateOfBirth { get; set; }
+        public Gender PersonGender { get; set; }
+
+        public enum Gender
+        {
+            Male, Female, Others
+        }
+    }
+}
