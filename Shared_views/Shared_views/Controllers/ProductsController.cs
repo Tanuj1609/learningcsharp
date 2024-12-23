664165b4c44481b21f70836223d2803ea3a2e038
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-24 05:23:01 +0530

    used for commonly shared views

---
+++ Shared_views/Shared_views/Controllers/ProductsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace viewimports.cshtml_in_RV.Controllers
+{
+    public class ProductsController : Controller
+    {   [Route("all-products")]
+        public IActionResult All()
+        {
+            return View();
+        }
+    }
+}
