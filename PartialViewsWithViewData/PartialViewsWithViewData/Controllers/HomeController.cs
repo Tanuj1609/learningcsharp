1a5da1cb15c34766538d6ecb27029896e961d715
Tanuj Tewatia <tanuj.tewatia@eptura.com>
2024-12-26 12:38:25 +0530

    use of view data and view bag in partial views

---
+++ PartialViewsWithViewData/PartialViewsWithViewData/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PartialViewsExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            ViewData["ListTitle"] = "Cities";
+            ViewData["ListItems"] = new List<string>()
+            {
+                "Paris","Germany","Rome","Gurugram"
+            };
+            return View();
+        }
+
+        [Route("About")]
+        public IActionResult About()
+        {
+            return View();
+        }
+    }
+}
