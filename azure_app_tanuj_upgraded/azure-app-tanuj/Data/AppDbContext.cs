9c84422cc3d6cf2bb72db417ce25caa3e7fdeae2
Tanuj1609 <tanujtewatia1609@gmail.com>
2025-02-19 01:08:13 +0530

    connected the previous app to ef core

---
+++ azure_app_tanuj_upgraded/azure-app-tanuj/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace azure_app_tanuj.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Person> Persons { get; set; }
+    }
+
+    
+}
